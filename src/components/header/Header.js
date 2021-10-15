f752da96bc7f1f7ee53e95e65f046131174c8060
Carolina-Lemes <caro@gmail.com>
2021-10-15 01:15:36 -0300

    actualización portfolio

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -3,9 +3,12 @@ import './Header.css'
 
 
 const Header = () => {
+
+ 
+
   return (  
             <header id="header" className="s-header  ">
-             <div className="container-fluid s-header__nav-wrap  sticky-top navbar-expand-lg" id="navbarTogglerDemo03">
+             <div className="container-fluid s-header__nav-wrap  sticky-top navbar-expand-lg navbar-collapse navbar-toggler" id="navbarTogglerDemo01">
               
              <nav className="s-header__nav d-flex" >
                 <ul  style={{alignItems:'center',justifyContent:'center', fontSize:'25px', padding:'0%', marggin:'0%', color:'#DEA9D3', backgroundColor:'black'}}>
@@ -17,9 +20,10 @@ const Header = () => {
                 </ul>
               </nav>
             </div>
-            <a className="s-header__menu-toggle navbar-expand-md" href="#index" title="Menu">
+            <a className=".s-header__nav s-header__menu-toggle navbar-expand-md navbar-toggler" href="#index" title="Menu">
               <span className="s-header__menu-icon" />
             </a>
+            
           </header>
         )
     }
--- src/components/portfolio/Portfolio.js
+++ src/components/portfolio/Portfolio.js
@@ -10,7 +10,7 @@ const Portfolio = () => {
       </div>
     </div>
     <br/>
-    <div className="row block-large-4-4 block-medium-4-4 block-tab-4-4 block-500-stack folio-list">
+    <div className="row block-large-1-3 block-medium-1-1 block-tab-1-1 block-600-stack folio-list">
       <div className="column folio-item">
         <a href="https://carolina053peke.github.io/random.user-fetch/" className="folio-item__thumb">
           <h6 className="titulo-proyectos"> UI-UX Fetch API</h6>
@@ -40,12 +40,48 @@ const Portfolio = () => {
       <div className="column folio-item">
         <a href="https://carolina053peke.github.io/fetch-poke-API/" className="folio-item__thumb">
           <img src="images/portfolio/sanddunes.jpg" srcSet="images/portfolio/sanddunes.jpg 1x, 
-                             images/portfolio/sanddunes@2x.jpg 2x" alt="boletin" />
+                             images/portfolio/sanddunes@2x.jpg 2x" alt="poke-api" />
           <h6 className="titulo-proyectos">Poke-API/Fetch</h6>
         </a>
         <br/>
       </div>
       <br/>
+      <div className="column folio-item">
+        <a href="https://carolina053peke.github.io/ChatBots-en-JavaScript/" className="folio-item__thumb">
+          <img src="images/portfolio/sanddunes.jpg" srcSet="images/portfolio/fuji5.jpg 1x, 
+                             images/portfolio/sanddunes@2x.jpg 2x" alt="ChatBots" />
+        <h6 className="titulo-proyectos">ChatBots-en-JavaScript</h6>
+        </a>
+        <br/>
+      </div>
+      <br/>
+      <div className="column folio-item">
+        <a href="https://carolina053peke.github.io/Notas_promedio_estudiantes/" className="folio-item__thumb">
+        <h6 className="titulo-proyectos">Notas-promedio-estudiantes</h6>
+          <img src="images/portfolio/sanddunes.jpg" srcSet="images/portfolio/fuji9.jpg 1x, 
+                             images/portfolio/sanddunes@2x.jpg 2x" alt="ChatBots" />
+        </a>
+        <br/>
+      </div>
+      <br/>
+      {/* <div className="column folio-item">
+        <a href="https://carolina053peke.github.io/Notas_promedio_estudiantes/" className="folio-item__thumb">
+          <img src="images/portfolio/sanddunes.jpg" srcSet="images/portfolio/fuji9.jpg 1x, 
+                             images/portfolio/sanddunes@2x.jpg 2x" alt="ChatBots" />
+        <h6 className="titulo-proyectos">Ecommerce-MyC</h6>
+        </a>
+        <br/>
+      </div>
+      <br/>
+      <div className="column folio-item">
+        <a href="https://carolina053peke.github.io/Notas_promedio_estudiantes/" className="folio-item__thumb">
+        <h6 className="titulo-proyectos">Ecommerce-MyC</h6>
+          <img src="images/portfolio/sanddunes.jpg" srcSet="images/portfolio/fuji9.jpg 1x, 
+                             images/portfolio/sanddunes@2x.jpg 2x" alt="ChatBots" />
+        </a>
+        <br/>
+      </div>
+      <br/> */}
     </div> 
   </section>
  
