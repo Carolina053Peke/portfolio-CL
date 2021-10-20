12885b680cbfe3e3afcdd405e1e88cf68e64cdef
Carolina <vamosconesasmanosalcodigo@gmail.com>
2021-10-19 21:58:50 -0300

    medias query

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -11,7 +11,7 @@ const About = () => {
     <div className="column large-3 tab-10">
       <img className="s-about__pic" src="images/avatars/user-1.jpg" alt="foto.cv"/>
       <div className="row s-about__content-bottom"  >
-            <div className="column w-1000-stack">
+            <div className="column w-1000-stack btn--download">
               <a href={CV} type="application/pdf" download="./Carolina_Lemes.pdf" className="btn btn--download">
                 <svg xmlns="" width={25} height={25} viewBox="0 0 24 24" style={{fill: 'rgba(0, 0, 0, 1)', transform: '', msFilter: ''}}><path d="M12 16L16 11 13 11 13 4 11 4 11 11 8 11z" /><path d="M20,18H4v-7H2v7c0,1.103,0.897,2,2,2h16c1.103,0,2-0.897,2-2v-7h-2V18z" /></svg>
                 DOWNLOAD CV
