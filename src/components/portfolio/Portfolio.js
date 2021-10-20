e44656d174ff545d1a17aa6ab075bf8ea10dfc5a
Carolina <vamosconesasmanosalcodigo@gmail.com>
2021-10-19 21:14:11 -0300

    botton ok

--- src/components/portfolio/Portfolio.js
+++ src/components/portfolio/Portfolio.js
@@ -10,11 +10,11 @@ const Portfolio = () => {
       </div>
     </div>
     <br/>
-    <div className="row block-large-4-4 block-medium-4-4 block-tab-4-4 block-500-stack folio-list">
+    <div className="row block-large-1-3 block-medium-1-1 block-tab-1-1 block-600-stack folio-list">
       <div className="column folio-item">
         <a href="https://carolina053peke.github.io/random.user-fetch/" className="folio-item__thumb">
           <h6 className="titulo-proyectos"> UI-UX Fetch API</h6>
-          <img src="images/portfolio/droplet.jpeg" srcSet="images/portfolio/rucksack.jpg 1x, 
+          <img src="images/portfolio/rucksack.jpeg" srcSet="images/portfolio/rucksack.jpg 1x, 
                              images/portfolio/rucksack@2x.jpg 2x" alt="cv" />
         </a>
         <br/>
@@ -22,9 +22,10 @@ const Portfolio = () => {
 
       <div className="column folio-item">
         <a href="https://carolina053peke.github.io/to_do_list/" className="folio-item__thumb">
-          <img src="images/portfolio/minimalismo.jpg" srcSet="images/portfolio/fuji.jpg 1x, 
+        <h6 className="titulo-proyectos">To do list</h6>  
+          <img src="images/portfolio/fuji.jpg" srcSet="images/portfolio/fuji.jpg 1x, 
                              images/portfolio/rucksack@2x.jpg 2x" alt="todolist" />
-          <h6 className="titulo-proyectos">To do list</h6>              
+                     
         </a>
         <br/>
       </div>
@@ -32,7 +33,7 @@ const Portfolio = () => {
       <div className="column folio-item">
         <a href="https://carolina053peke.github.io/proyecto-calculadora-jQuery/" className="folio-item__thumb">
         <h6 className="titulo-proyectos">Calculadora en Jquery</h6>
-          <img src="images/portfolio/rucksack.jpg" srcSet="images/portfolio/lamp.jpg 1x, 
+          <img src="images/portfolio/lamp.jpg" srcSet="images/portfolio/lamp.jpg 1x, 
                              images/portfolio/rucksack@2x.jpg 2x" alt="calcyladora" />
         </a>
         <br/>
@@ -40,14 +41,52 @@ const Portfolio = () => {
       <div className="column folio-item">
         <a href="https://carolina053peke.github.io/fetch-poke-API/" className="folio-item__thumb">
           <img src="images/portfolio/sanddunes.jpg" srcSet="images/portfolio/sanddunes.jpg 1x, 
-                             images/portfolio/sanddunes@2x.jpg 2x" alt="boletin" />
+                             images/portfolio/sanddunes@2x.jpg 2x" alt="poke-api" />
           <h6 className="titulo-proyectos">Poke-API/Fetch</h6>
         </a>
         <br/>
       </div>
       <br/>
+      <div className="column folio-item">
+        <a href="https://carolina053peke.github.io/ChatBots-en-JavaScript/" className="folio-item__thumb">
+          <img src="images/portfolio/fuji5.jpg" srcSet="images/portfolio/fuji5.jpg 1x, 
+                             images/portfolio/sanddunes@2x.jpg 2x" alt="ChatBots" />
+        <h6 className="titulo-proyectos">ChatBots-en-JavaScript</h6>
+        </a>
+        <br/>
+      </div>
+      <br/>
+      <div className="column folio-item">
+        <a href="https://carolina053peke.github.io/Notas_promedio_estudiantes/" className="folio-item__thumb">
+        
+          <img src="images/portfolio/fuji9.jpg" srcSet="images/portfolio/fuji9.jpg 1x, 
+                             images/portfolio/sanddunes@2x.jpg 2x" alt="ChatBots" />
+          <h6 className="titulo-proyectos">Notas-promedio</h6>
+        </a>
+        <br/>
+      </div>
+      <br/>
+      {/* <div className="column folio-item">
+        <a href="https://carolina053peke.github.io/Notas_promedio_estudiantes/" className="folio-item__thumb">
+          <img src="images/portfolio/sanddunes.jpg" srcSet="images/portfolio/fuji9.jpg 1x, 
+                             images/portfolio/sanddunes@2x.jpg 2x" alt="ChatBots" />
+        <h6 className="titulo-proyectos">Ecommerce-MyC</h6>
+        </a>
+        <br/>
+      </div>
+      <br/>
+      <div className="column folio-item">
+        <a href="https://carolina053peke.github.io/Notas_promedio_estudiantes/" className="folio-item__thumb">
+        <h6 className="titulo-proyectos">Ecommerce-MyC</h6>
+          <img src="images/portfolio/sanddunes.jpg" srcSet="images/portfolio/fuji9.jpg 1x, 
+                             images/portfolio/sanddunes@2x.jpg 2x" alt="ChatBots" />
+        </a>
+        <br/>
+      </div>
+      <br/> */}
     </div> 
   </section>
+ 
  
     )
   }
